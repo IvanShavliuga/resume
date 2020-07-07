9f92cc2aafb486b19224ff66af5011689cb63999
Ivan <iva.drakon.nov@gmail.com>
2020-07-07 17:21:35 +0200

    Vue-cli stability version

--- resumenew/src/main.js
+++ resumenew/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import './assets/css/style.less'
+import './assets/css/tablet.css'
+import './assets/css/mobile.css'
+import './assets/css/print.css'
 Vue.config.productionTip = false
 
 new Vue({
